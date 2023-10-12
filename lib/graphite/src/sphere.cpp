a10710c2577cb8b1e3935672844c49edc70fbfde
André Willyan <andrewillyan@tutanota.com>
2023-10-12 16:12:05 -0300

    fix sphere intersection detection

--- lib/graphite/src/sphere.cpp
+++ lib/graphite/src/sphere.cpp
@@ -1,5 +1,8 @@
 #include "../include/sphere.hpp"
+#include "../../algebrick/include/point3d.hpp"
+#include "../../algebrick/include/ray.hpp"
 #include <SDL2/SDL_pixels.h>
+#include <algorithm>
 #include <cmath>
 #include <utility>
 
@@ -13,11 +16,14 @@ std::optional<PointColor> Sphere::intersect(const Algebrick::Ray &ray) const {
   Algebrick::Vec3d w = ray.source() - center;
   double w_dr = w * ray.direction();
   double b = 2 * w_dr;
-  double delta = std::pow(w_dr, 2) - ((w * w) - std::pow(radius, 2));
+  double c = (w * w) - (std::pow(radius, 2));
+  double delta = std::pow(b, 2) - (4 * c);
+
   if (delta < 0)
     return {};
 
-  double ray_inter_scalar = (-b + std::sqrt(delta)) / 2;
+  double ray_inter_scalar =
+      std::min((-b + std::sqrt(delta)) / 2.0, (-b - std::sqrt(delta)) / 2.0);
   Algebrick::Point3d inter_point =
       Algebrick::Point3d(ray.direction() * ray_inter_scalar);
   return std::make_pair(std::move(inter_point), color);
