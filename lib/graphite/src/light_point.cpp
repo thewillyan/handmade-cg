df365b32b6b76bb774341461afa183ab39c16aa5
André Willyan <andrewillyan@tutanota.com>
2023-10-29 17:16:22 -0300

    fix reflection and sphere intersection

--- lib/graphite/src/light_point.cpp
+++ lib/graphite/src/light_point.cpp
@@ -2,6 +2,7 @@
 #include "../../algebrick/include/point3d.hpp"
 #include "../include/object.hpp"
 #include "algebrick/include/ray.hpp"
+#include "algebrick/include/vec3d.hpp"
 #include <cmath>
 #include <stdexcept>
 
@@ -18,33 +19,30 @@ Intensity Point::get_intensity(const Object &inter_obj,
                                std::vector<Object *> objs,
                                const PointColor &inter,
                                const Algebrick::Ray &eye_ray) const {
-  Algebrick::Ray light_ray{inter.first, p};
-  for (auto &obj : objs) {
-    if (obj != &inter_obj && obj->intersect(light_ray).has_value())
-      return {0, 0, 0};
-  };
-  // Algebrick::Ray light_ray{inter.first, p};
-  // for (auto &obj : objs) {
-  //   if (obj != &inter_obj) {
-  //     auto other_inter = obj->intersect(light_ray);
-  //     if (other_inter.has_value()) {
-  //       double distance_inter_to_light = (p - inter.first).length();
-  //       double distance_another_inter_to_light =
-  //           (p - other_inter->first).length();
-  //       if (distance_inter_to_light >= distance_another_inter_to_light) {
-  //         return {0, 0, 0};
-  //       };
-  //     }
-  //   }
-  // }
 
+  Algebrick::Vec3d L = (p - inter.first);
+  double ray_len = L.length();
+
+  Algebrick::Ray light_ray{p, inter.first};
+  for (auto &obj : objs) {
+    if (obj != &inter_obj) {
+      auto other_inter = obj->intersect(light_ray);
+      if (other_inter.has_value()) {
+        Algebrick::Vec3d other_inter_vec = (other_inter->first - p);
+        double other_inter_len = other_inter_vec.length();
+        if (ray_len >= other_inter_len) {
+          return {0, 0, 0};
+        }
+      }
+    }
+  }
+
+  Algebrick::Vec3d l = L.norm();
   auto n = inter_obj.normal(inter.first);
   if (!n.has_value())
-    throw std::invalid_argument("Invalid intesection point!");
+    return {0, 0, 0};
 
   Algebrick::Vec3d v = -eye_ray.direction();
-  Algebrick::Vec3d L = (p - inter.first);
-  Algebrick::Vec3d l = L.norm();
   Algebrick::Vec3d r = ((*n) * ((*n) * l * 2)) - l;
 
   Intensity i_dif = (i * inter_obj.get_dif_int()) * (l * (*n));
