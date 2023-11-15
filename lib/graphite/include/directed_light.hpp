84047edc193d0a02c7812d82149feb74b62d57c0
taldoflemis <coderflemis@gmail.com>
2023-11-15 20:51:18 -0300

    feat: add directed light

---
+++ lib/graphite/include/directed_light.hpp
@@ -0,0 +1,29 @@
+#ifndef Graphite_Directed_Light
+#define Graphite_Directed_Light
+
+#include "../../algebrick/include/point3d.hpp"
+#include "../../algebrick/include/vec3d.hpp"
+#include "light.hpp"
+
+namespace Graphite::Light {
+
+class Directed : public Source {
+private:
+  Algebrick::Point3d p;
+  Intensity i;
+  Algebrick::Vec3d decay;
+  Algebrick::Vec3d direction;
+
+public:
+  Directed(Algebrick::Point3d point, Intensity base_intensity,
+           Algebrick::Vec3d direction);
+
+  Intensity get_intensity(const Object &obj, std::vector<Object *> objs,
+                          const PointColor &inter,
+                          const Algebrick::Ray &eye_ray) const override;
+  void set_decay(double a, double b, double c);
+};
+
+} // namespace Graphite::Light
+
+#endif // !Graphite_Directed_Light
