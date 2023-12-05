0e44dc1d106c14177990c68b6d015be831a2b9fa
taldoflemis <coderflemis@gmail.com>
2023-12-05 20:08:27 -0300

    refac: add obj intensity class

---
+++ lib/graphite/src/objs/obj_intensity.cpp
@@ -0,0 +1,49 @@
+#include "graphite/include/objs/obj_intensity.hpp"
+
+Graphite::Object::ObjectIntensity::ObjectIntensity()
+    : shineness(1), ambient_intensity(1, 1, 1), diffuse_intensity(1, 1, 1),
+      specular_intensity(1, 1, 1){};
+
+Graphite::Object::ObjectIntensity::ObjectIntensity(
+    double shineness, const Light::Intensity &ambient_intensity,
+    const Light::Intensity &diffuse_intensity,
+    const Light::Intensity &specular_intensity)
+    : shineness(shineness), ambient_intensity(ambient_intensity),
+      diffuse_intensity(diffuse_intensity),
+      specular_intensity(specular_intensity){};
+
+// methods
+double Graphite::Object::ObjectIntensity::get_shineness() const {
+  return shineness;
+}
+Graphite::Light::Intensity
+Graphite::Object::ObjectIntensity::get_ambient_intensity() const {
+  return ambient_intensity;
+}
+Graphite::Light::Intensity
+Graphite::Object::ObjectIntensity::get_diffuse_intensity() const {
+  return diffuse_intensity;
+}
+Graphite::Light::Intensity
+Graphite::Object::ObjectIntensity::get_specular_intensity() const {
+  return specular_intensity;
+}
+
+void Graphite::Object::ObjectIntensity::set_shineness(double shi) {
+  this->shineness = shi;
+}
+
+void Graphite::Object::ObjectIntensity::set_ambient_intensity(
+    const Light::Intensity &amb) {
+  this->ambient_intensity = amb;
+}
+
+void Graphite::Object::ObjectIntensity::set_diffuse_intensity(
+    const Light::Intensity &dif) {
+  this->diffuse_intensity = dif;
+}
+
+void Graphite::Object::ObjectIntensity::set_specular_intensity(
+    const Light::Intensity &spec) {
+  this->specular_intensity = spec;
+}
