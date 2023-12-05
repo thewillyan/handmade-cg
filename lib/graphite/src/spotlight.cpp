ac39f331b94c44c20c71deb8a7f1a18a1882e70c
taldoflemis <coderflemis@gmail.com>
2023-12-05 20:42:10 -0300

    refac: modify lights to match the new ObjIntensity color

--- lib/graphite/src/spotlight.cpp
+++ lib/graphite/src/spotlight.cpp
@@ -1,11 +1,12 @@
 #include "graphite/include/spotlight.hpp"
+#include "graphite/include/objs/obj_intensity.hpp"
 
 using namespace Graphite::Light;
 
 Spot::Spot(Algebrick::Point3d point, Intensity base_intensity,
            Algebrick::Vec3d direction, double angle)
-    : p{point}, i{base_intensity}, direction{direction.norm()}, aperture{
-                                                                    angle} {}
+    : p{point}, i{base_intensity}, direction{direction.norm()},
+      aperture{angle} {}
 
 Intensity Spot::get_intensity(const Object::Object &inter_obj,
                               std::vector<Object::Object *> objs,
@@ -40,16 +41,17 @@ Intensity Spot::get_intensity(const Object::Object &inter_obj,
   if (!n.has_value())
     return {0, 0, 0};
 
+  Object::ObjectIntensity oi = inter_obj.get_intensity(inter.first);
   Algebrick::Vec3d v = -eye_ray.direction();
   Algebrick::Vec3d r = ((*n) * ((*n) * l * 2)) - l;
 
-  Intensity i_dif = (i * inter_obj.get_dif_int()) * (l * (*n));
-  double reflect = inter_obj.get_reflection();
+  Intensity i_dif = (i * oi.get_diffuse_intensity()) * (l * (*n));
+  double reflect = oi.get_shineness();
   double rv = r * v;
 
   Intensity i_esp{0, 0, 0};
   if (rv > 0) {
-    i_esp = (i * inter_obj.get_espec_int()) * std::pow(r * v, reflect);
+    i_esp = (i * oi.get_specular_intensity()) * std::pow(r * v, reflect);
   }
 
   return (i_dif + i_esp);
