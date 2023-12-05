a2773b1d865fa7d57e5d25b36065a8d415dced4a
taldoflemis <coderflemis@gmail.com>
2023-12-05 08:35:20 -0300

    fix: directed light using point kkkk

--- lib/graphite/include/directed_light.hpp
+++ lib/graphite/include/directed_light.hpp
@@ -1,7 +1,6 @@
 #ifndef Graphite_Directed_Light
 #define Graphite_Directed_Light
 
-#include "../../algebrick/include/point3d.hpp"
 #include "../../algebrick/include/vec3d.hpp"
 #include "light.hpp"
 
@@ -9,14 +8,12 @@ namespace Graphite::Light {
 
 class Directed : public Source {
 private:
-  Algebrick::Point3d p;
   Intensity i;
   Algebrick::Vec3d decay;
   Algebrick::Vec3d direction;
 
 public:
-  Directed(Algebrick::Point3d point, Intensity base_intensity,
-           Algebrick::Vec3d direction);
+  Directed(Intensity base_intensity, Algebrick::Vec3d direction);
 
   Intensity get_intensity(const Object &obj, std::vector<Object *> objs,
                           const PointColor &inter,
